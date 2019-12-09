25d8218c23721a1bc6b5d7a390ffabaefb18241e
Jong-Seok-Lee <ljs8049@naver.com>
2019-12-09 21:03:31 +0900

    Change End Point of mqtt_shadow host

--- server/mqtt_shadow.py
+++ server/mqtt_shadow.py
@@ -5,10 +5,10 @@ from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient  # need to install AW
 
 Client_ID = "CoAP_Client"
 Thing_Name = "IoT_System"  #Device ID
-Host_Name = "a1zm4oahh01g1s-ats.iot.ap-northeast-1.amazonaws.com"   #End Point
+Host_Name = "a2zom7jv3fdwvd-ats.iot.ap-southeast-2.amazonaws.com"   #End Point
 Root_CA = "./cert2/RootCA.crt"
-Private_Key = "./cert2/cc544bb473-private.pem.key"
-Cert_File = "./cert2/cc544bb473-certificate.pem.crt"
+Private_Key = "./cert2/b603b692cf-private.pem.key"
+Cert_File = "./cert2/b603b692cf-certificate.pem.crt"
 
 Client = AWSIoTMQTTShadowClient(Client_ID)
 Client.configureEndpoint(Host_Name, 8883)
@@ -25,22 +25,25 @@ def Callback_func(payload, responseStatus, token):
     print("payload = " + payload)
     print("responseStatus = " + responseStatus)
     print("token = " + token)
-    
+
 while True:
     fp = open("dustDat.txt", "r+")
-    line_list=[]
+    #line_list=[]
+    line_tmp = fp.readline()
+
     while True:
         line = fp.readline()
         if not line:    # If line is last line
             break
-
-        #Save selected data to line_list
+        
+        #Save last selected data to line_tmp
         dic_ln = json.loads(line)
-        if int(dic_ln['minor_num']) >= 1 and int(dic_ln['minor_num']) <= 3:
-            line_list.append(json.dumps(dic_ln))
+        if int(dic_ln['minor_num']) == 3:
+            #line_list.append(json.dumps(dic_ln))
+            line_tmp = line
 
-    msg = {"state": {"reported": {"dust_data": line_list}}}
-    #print(line_list)
+    #msg = {"state": {"reported": {"dust_data": line_list}}}
+    msg = {"state": {"reported": {"dust_data": json.loads(line_tmp)}}}
     Device.shadowUpdate(json.dumps(msg), Callback_func, 5)
 
     fp.close
